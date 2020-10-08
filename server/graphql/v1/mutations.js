2b72468be27441cbbfd79e10b4b3ad0dac5c247d
Benjamin Piouffle <benjamin@opencollective.com>
2020-10-08 10:27:19 +0200

    feat: Guest contributions

--- server/graphql/v1/mutations.js
+++ server/graphql/v1/mutations.js
@@ -464,6 +464,7 @@ const mutations = {
   },
   createOrder: {
     type: OrderType,
+    deprecationReason: '2020-10-13: This endpoint has been moved to GQLV2',
     args: {
       order: {
         type: new GraphQLNonNull(OrderInputType),
